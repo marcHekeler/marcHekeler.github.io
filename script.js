51adf29d91e1acaceaaf5e289b7c7fcc10414ba8
Marc Hekeler <marc.hekeler@jambit.com>
2023-01-22 14:34:58 +0100

    Snake steering

--- script.js
+++ script.js
@@ -13,11 +13,36 @@ const snake = {
         {x: 0, y:0}
     ],
     length: 4,
+    direction: 'r', // 'l', 'r', 'u', 'd'
 }
 
 function fillCell({x, y}, color) {
     context.fillStyle = color;
     context.fillRect(x * cellLength, y * cellLength, cellLength - 1, cellLength - 1);
+}
+
+function getCurrentPosition() {
+    return snake.cells[0];
+}
+
+function getNewX() {
+    if(snake.direction === 'r') {
+        return getCurrentPosition().x === 24 ? 0 : getCurrentPosition().x + 1;
+    }
+    if(snake.direction === 'l') {
+        return getCurrentPosition().x === 0 ? 24 : getCurrentPosition().x - 1;
+    }
+    return getCurrentPosition().x;
+}
+
+function getNewY() {
+    if(snake.direction === 'd') {
+        return getCurrentPosition().y === 24 ? 0 : getCurrentPosition().y + 1;
+    }
+    if(snake.direction === 'u') {
+        return getCurrentPosition().y === 0 ? 24 : getCurrentPosition().y - 1;
+    }
+    return getCurrentPosition().y;
 }
 
 function moveSnake() {
@@ -35,7 +60,7 @@ function moveSnake() {
     const newY = snake.cells[0].y;
 
     // add new cell to sake
-    snake.cells.unshift({x: newX, y: newY});
+    snake.cells.unshift({x: getNewX(), y: getNewY()});
     snake.cells.pop();
 
     // draw current cell
@@ -43,6 +68,28 @@ function moveSnake() {
 
     window.requestAnimationFrame(moveSnake);
 }
+
+document.addEventListener('keydown', function(e) {
+    console.log(e);
+    switch(e.key) {
+        // left
+        case 'ArrowLeft':
+            if(snake.direction !== 'r') snake.direction = 'l';
+            break;
+        // up
+        case 'ArrowUp':
+            if(snake.direction !== 'd') snake.direction = 'u';
+            break;
+        // right
+        case 'ArrowRight':
+            if(snake.direction !== 'l') snake.direction = 'r';
+            break;
+        // down
+        case 'ArrowDown':
+            if(snake.direction !== 'u') snake.direction = 'd';
+            break;
+    };
+});
 
 // start
 window.requestAnimationFrame(moveSnake);
