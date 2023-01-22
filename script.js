d8fc2176511440fe02fa20a506aef9d3905a131f
Marc Hekeler <marc.hekeler@jambit.com>
2023-01-22 13:06:40 +0100

    snake with length

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ var cellLength = 16;
 var speedCounter = 0;
 
 const snake = {
-    x: 0,
-    y: 0,
+    cells: [
+        {x: 3, y:0},
+        {x: 2, y:0},
+        {x: 1, y:0},
+        {x: 0, y:0}
+    ],
+    length: 4,
 }
 
-function fillCell(x, y, color) {
+function fillCell({x, y}, color) {
     context.fillStyle = color;
     context.fillRect(x * cellLength, y * cellLength, cellLength - 1, cellLength - 1);
 }
@@ -23,9 +28,12 @@ function moveSnake() {
     }
     speedCounter = 0;
 
-    fillCell(snake.x, snake.y, 'black');
-    snake.x = snake.x === 24 ? 0 : snake.x + 1;
-    fillCell(snake.x, snake.y, 'green');
+    fillCell(snake.cells[snake.length-1], 'black');
+    const newX = snake.cells[0].x === 24 ? 0 : snake.cells[0].x +1;
+    const newY = snake.cells[0].y;
+    snake.cells.unshift({x: newX, y: newY});
+    snake.cells.pop();
+    fillCell(snake.cells[0], 'green');
     window.requestAnimationFrame(moveSnake);
 }
 
