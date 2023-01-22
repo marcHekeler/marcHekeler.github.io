edd51808a2c4fef3300aea061cb4654e26f311ef
Marc Hekeler <marc.hekeler@jambit.com>
2023-01-22 11:39:13 +0100

    move snake

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ var context = canvas.getContext('2d');
 
 var squareLength = 16;
 
+const snake = {
+    x: 0,
+    y: 0,
+}
+
+function clearCanvas() {
+  context.clearRect(0,0,canvas.width,canvas.height);
+}
+
 function fillSquare(x, y, color) {
     context.fillStyle = color;
     context.fillRect(x * squareLength, y * squareLength, squareLength - 1, squareLength - 1);
 }
 
-// start 
-fillSquare(0, 0, 'green');
+function moveSnake() {
+
+    clearCanvas();
+    snake.x = snake.x === 24 ? 0 : snake.x + 1;
+    fillSquare(snake.x, snake.y, 'green');
+    window.requestAnimationFrame(moveSnake);
+}
+
+// start
+window.requestAnimationFrame(moveSnake);
