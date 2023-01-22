6ec8638eecd98f7f16d96ff34c6c3cfcc8e49942
Marc Hekeler <marc.hekeler@jambit.com>
2023-01-22 10:30:29 +0100

    add first square

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
+
+var squareLength = 16;
+
+context.fillStyle = 'green';
+context.fillRect(0, 0, squareLength - 1, squareLength - 1);
