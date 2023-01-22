32e3cf918a35134ffd69351faf70e3479b6ce314
Marc Hekeler <marc.hekeler@jambit.com>
2023-01-22 10:52:53 +0100

    refactoring: use function to draw square

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// definitions
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
 var squareLength = 16;
 
-context.fillStyle = 'green';
-context.fillRect(0, 0, squareLength - 1, squareLength - 1);
+function fillSquare(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x * squareLength, y * squareLength, squareLength - 1, squareLength - 1);
+}
+
+// start 
+fillSquare(0, 0, 'green');
